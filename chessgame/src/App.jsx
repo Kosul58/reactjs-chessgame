d389b675951c8e4d5642585435314eb7af1bd55c
Kosul58 <kosulgrg@gmail.com>
2025-01-29 09:26:43 +0545

    qa

--- chessgame/src/App.jsx
+++ chessgame/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import bk from "./assets/bk.png";
 import bb from "./assets/bb.png";
@@ -804,7 +804,7 @@ function App() {
 
   useEffect(() => {
     checkassure2();
-  }, [board]);
+  }, [board, checkassure2]);
 
   return (
     <div className="chessgame">
